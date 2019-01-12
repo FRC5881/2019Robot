0fa618f8987a58accedd2ec364bbd3b0819c39d7
rnagothi <rnagothi@techvalleyhigh.org>
2019-01-12 09:36:32 -0500

    Started arm subsystem

---
+++ src/main/java/org/techvalleyhigh/frc5881/deepspace/robot/subsystem/Arm.java
@@ -0,0 +1,23 @@
+package org.techvalleyhigh.frc5881.deepspace.robot.subsystem;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class Arm {
+
+  private static WPI_TalonSRX armMotor = new WPI_TalonSRX(5);
+
+  public Arm (){
+    super ();
+
+    init ();
+  }
+
+  public void init() {
+
+  }
+
+
+
+
+
+}
