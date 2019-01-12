de57cfa3d4d4e795a58fba062a360c370a3d154f
rnagothi <rnagothi@techvalleyhigh.org>
2019-01-12 15:53:06 -0500

    I did the Java Docs for the arm.

--- src/main/java/org/techvalleyhigh/frc5881/deepspace/robot/subsystem/Arm.java
+++ src/main/java/org/techvalleyhigh/frc5881/deepspace/robot/subsystem/Arm.java
@@ -9,8 +9,10 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 public class Arm extends Subsystem {
 
+  //Define motor
   private static WPI_TalonSRX armMotor = new WPI_TalonSRX(5);
 
+  //The maximum and minimum numbers of ticks for how far the arm can move
   private static final int maxTicks = 0;
   private static final int minTicks = 200;
 
@@ -23,11 +25,15 @@ public class Arm extends Subsystem {
   protected void initDefaultCommand() {
   }
 
+  /**Create the subsystem with a default name*/
   public Arm (){
     super ();
     init ();
   }
 
+/**
+ * Initialize SmartDashboard and other local variables
+ */
   public void init() {
     armMotor.setName("Arm", "Motor");
     armMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
@@ -35,6 +41,11 @@ public class Arm extends Subsystem {
     LiveWindow.add(armMotor);
   }
 
+  /**
+   * Set the setPoint to stay in between the minimum and maximum amount of ticks
+   * If setPoint is less than minTicks, it will be equal to minTicks
+   * If setPoint is more than maxTicks, it will be equal to maxTicks
+  */
   public void setArmMotor (double setPoint){
     if (setPoint < minTicks) setPoint = minTicks;
     if (setPoint > maxTicks) setPoint = maxTicks;
@@ -42,6 +53,9 @@ public class Arm extends Subsystem {
     armMotor.set(ControlMode.Position, setPoint);
   }
 
+  /**
+   * set arm to target cargo or hatch
+   */
   public void setToCargoTicks(){
     setArmMotor(cargoTicks);
   }
@@ -50,6 +64,9 @@ public class Arm extends Subsystem {
     setArmMotor(hatchTicks);
   }
 
+  /**
+   * flip arm setpoint from cargo to hatch and vice versa
+   */
   public void flip() {
     if(getSetPoint() == hatchTicks) {
       setToCargoTicks();
@@ -58,6 +75,10 @@ public class Arm extends Subsystem {
     }
   }
 
+  /**
+   * identifies which mode it is in, cargo or hatch
+   * returns if it's true or false
+   */
   public boolean isCargo() {
     return getSetPoint() == cargoTicks;
   }
@@ -66,9 +87,8 @@ public class Arm extends Subsystem {
     return getSetPoint() == hatchTicks;
   }
 
+
   public double getSetPoint() {
     return armMotor.getClosedLoopTarget();
   }
-
-
 }
