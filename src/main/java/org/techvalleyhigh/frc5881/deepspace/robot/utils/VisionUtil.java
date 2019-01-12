5dc16da51bffc135fd2fa43ea14db24f290f4343
nberman <nberman@techvalleyhigh.org>
2019-01-12 16:30:43 -0500

    Started Vision math in VisionUtil

--- src/main/java/org/techvalleyhigh/frc5881/deepspace/robot/utils/VisionUtil.java
+++ src/main/java/org/techvalleyhigh/frc5881/deepspace/robot/utils/VisionUtil.java
@@ -1,4 +1,19 @@
 package org.techvalleyhigh.frc5881.deepspace.robot.utils;
 
 public class VisionUtil {
+  double getTapeAngle(double ax, double ay, double bx, double by) {
+    return Math.atan((bx - ax)/(by - ay));
+  }
+
+  double getNeededAngle(double dist, double tapeAngle) {
+    return Math.asin((18 * Math.sin(Math.PI - tapeAngle))/dist);
+  }
+
+  double getDX(double a, double dist) {
+    return Math.sin(a) * dist;
+  }
+
+  double getDY(double a, double dist) {
+    return Math.cos(a) * dist;
+  }
 }
