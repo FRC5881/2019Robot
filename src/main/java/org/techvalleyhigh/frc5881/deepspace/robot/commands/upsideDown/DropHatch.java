e097511ba67cd03f81449cbf6d985d70a1dd77c5
Jacob <jhausgaard@techvalleyhigh..org>
2019-01-25 15:34:59 -0500

    Fixed JavaDoc in DropHatch, added JavaDocs in GrabHatch and UpsideDownFlip. Also added the corresponding commands to OI for grabbing and dropping a hatch.

--- src/main/java/org/techvalleyhigh/frc5881/deepspace/robot/commands/upsideDown/DropHatch.java
+++ src/main/java/org/techvalleyhigh/frc5881/deepspace/robot/commands/upsideDown/DropHatch.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.command.InstantCommand;
 import org.techvalleyhigh.frc5881.deepspace.robot.Robot;
 
 /**
- * its runnable
+ * It's runnable
  */
 public class DropHatch extends InstantCommand {
   private static Runnable runnable = () -> Robot.upsideDown.dropHatch();
--- src/main/java/org/techvalleyhigh/frc5881/deepspace/robot/commands/upsideDown/UpsideDownFlip.java
+++ src/main/java/org/techvalleyhigh/frc5881/deepspace/robot/commands/upsideDown/UpsideDownFlip.java
@@ -3,6 +3,9 @@ package org.techvalleyhigh.frc5881.deepspace.robot.commands.upsideDown;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import org.techvalleyhigh.frc5881.deepspace.robot.Robot;
 
+/**
+ * Changes the "state" of Upside Down.
+ */
 public class UpsideDownFlip extends InstantCommand {
   private static Runnable runnable = () -> Robot.upsideDown.toggle();
 
