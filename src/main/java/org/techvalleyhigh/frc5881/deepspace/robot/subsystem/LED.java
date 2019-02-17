216b68c7309a4ed85e96af1960ed5e5d9bb7705a
cmahoney1 <alextopher1@gmail.com>
2019-02-17 16:47:32 -0500

    Worked on LED

--- src/main/java/org/techvalleyhigh/frc5881/deepspace/robot/subsystem/LED.java
+++ src/main/java/org/techvalleyhigh/frc5881/deepspace/robot/subsystem/LED.java
@@ -1,8 +1,39 @@
 package org.techvalleyhigh.frc5881.deepspace.robot.subsystem;
 
+import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class LED extends Subsystem {
+
+  /**
+   * Variable holds the current state of the elevator
+   */
+  private LEDState state = LEDState.DISABLED;
+
+  /**
+   * Our alliance color
+   */
+  private DriverStation.Alliance color;
+
+
+  enum LEDState {
+    DISABLED(5),
+    TELEOP(10),
+    LIFTUP(20),
+    LIFTDOWN(30),
+    MANIPULATING(40);
+
+    private double value;
+
+    LEDState(double value) {
+      this.value = value;
+    }
+
+    public double getValue() {
+      return value;
+    }
+  }
 
   public LED() {
     // Run default subsystem constructor
@@ -21,14 +52,14 @@ public class LED extends Subsystem {
   protected void initDefaultCommand() {
   }
 
-  public void ledDocking(){
-    // TODO: Code this.
-  }
-
   /**
    * Init subsystem variables
    */
   private void init() {
+    color = DriverStation.getInstance().getAlliance();
   }
 
+  public void displayColor() {
+
+  }
 }
