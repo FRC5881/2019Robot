b0ac6eddfa41e99fe2eb55f65d84136244328015
Joshua Lamont <jlamont@techvalleyhigh.org>
2019-10-05 15:27:15 -0400

    Rumble Drive.

---
+++ src/main/java/org/techvalleyhigh/frc5881/deepspace/robot/DriveControl.java
@@ -0,0 +1,106 @@
+package org.techvalleyhigh.frc5881.deepspace.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class DriveControl extends Subsystem {
+    // Define motors
+    public static WPI_TalonSRX frontLeftMotor = new WPI_TalonSRX(2);
+    public static WPI_TalonSRX frontRightMotor = new WPI_TalonSRX(4);
+    public static WPI_TalonSRX backLeftMotor = new WPI_TalonSRX(1);
+    public static WPI_TalonSRX backRightMotor = new WPI_TalonSRX(3);
+
+    private static final String X_AXIS_SENSITIVITY = "X axis sensitivity";
+    private static final String Y_AXIS_SENSITIVITY = "Y axis sensitivity";
+
+    // Define robot drive for controls
+    private DifferentialDrive robotDrive;
+
+    /**
+     * Starts a command on init of subsystem, defining commands in robot and OI is preferred
+     */
+    @Override
+    protected void initDefaultCommand() {
+        setDefaultCommand(new ArcadeDrive());
+    }
+
+    /**
+     * Create the subsystem with a default name
+     */
+    public DriveControl() {
+        super();
+
+        init();
+    }
+
+    /**
+     * Initialize SmartDashboard and other local variables
+     */
+    public void init() {
+        frontLeftMotor.configFactoryDefault();
+        frontRightMotor.configFactoryDefault();
+        backLeftMotor.configFactoryDefault();
+        backRightMotor.configFactoryDefault();
+
+        frontLeftMotor.setName("Drive", "Front Left");
+        //frontLeftMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+        //frontLeftMotor.setInverted(true);
+        LiveWindow.add(frontLeftMotor);
+
+        frontRightMotor.setName("Drive", "Front Right");
+        //frontRightMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+        //frontRightMotor.setInverted(true);
+        LiveWindow.add(frontRightMotor);
+
+        backLeftMotor.setName("Drive", "Back Left");
+        //backLeftMotor.set(ControlMode.Follower, 1);
+        //backLeftMotor.setInverted(true);
+        LiveWindow.add(backLeftMotor);
+
+        backRightMotor.setName("Drive", "Back Right");
+        //backRightMotor.set(ControlMode.Follower, 2);
+        //backRightMotor.setInverted(true);
+        LiveWindow.add(backRightMotor);
+
+        SmartDashboard.putNumber(X_AXIS_SENSITIVITY, .8);
+        SmartDashboard.putNumber(Y_AXIS_SENSITIVITY, -1);
+
+        SpeedControllerGroup m_left = new SpeedControllerGroup(frontLeftMotor, backLeftMotor);
+        SpeedControllerGroup m_right = new SpeedControllerGroup(frontRightMotor, backRightMotor);
+        robotDrive = new DifferentialDrive(m_left, m_right);
+
+    }
+
+    /**
+     * Pass raw values to arcade drive, don't pass joystick inputs directly
+     * @param speed Drive speed -1 backwards -> 1 forward
+     * @param turn Turn rate -1 left -> 1 right
+     */
+    public void rawArcadeDrive(double speed, double turn){
+        robotDrive.arcadeDrive(speed, turn, true);
+    }
+
+    /**
+     * Implements arcade drive with joystick inputs
+     */
+    public void arcadeJoystickInputs () {
+        double speed = Robot.oi.driverController.getRawAxis(OI.XBOX_LEFT_Y_AXIS) * -1;
+        double turn = Robot.oi.driverController.getRawAxis(OI.XBOX_RIGHT_X_AXIS);
+
+        if(Math.abs(turn) < 0.1) {
+            turn = 0;
+        }
+        if(Math.abs(speed) < 0.1) {
+            speed = 0;
+        }
+
+        rawArcadeDrive(speed, turn);
+    }
+}
